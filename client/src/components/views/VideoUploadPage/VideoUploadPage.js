fc2cab1c72811bb5c4fad6820323d120c93e2ef7
yeonghoon Park <huklmnj2@naver.com>
2023-04-10 21:35:14 +0900

    first commit

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Typography, Button, Form, message, Input, Icon } from "antd";
+import Dropzone from "react-dropzone";
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+function VideoUploadPage() {
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <Title level={2}>Upload Video </Title>
+      </div>
+
+      <Form>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          {/* Drop zone */}
+          <Dropzone onDrop multiple maxSize>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  width: "300px",
+                  height: "240px",
+                  border: "1px solid lightgray",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <Icon type='plus' style={{ fontSize: "3rem" }} />
+              </div>
+            )}
+          </Dropzone>
+
+          {/* Thumbname */}
+          <div>
+            <img src='' alt=''></img>
+          </div>
+        </div>
+
+        <br />
+        <br />
+        <label>Title</label>
+        <Input />
+
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea />
+
+        <br />
+        <br />
+        <select>
+          <option></option>
+        </select>
+
+        <br />
+        <br />
+        <select>
+          <option></option>
+        </select>
+
+        <br />
+        <br />
+        <Button type='primary' size='large'>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default VideoUploadPage;
